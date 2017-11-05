ee5c0437606646e3c0717890d92f006d3bc42e6a
AleksTor2208 <aleksander.toropowski@gmail.com>
2017-11-05 23:52:03 +0300

    Add default mvc project

---
+++ SmothieDelight_RecruitmentAssignment/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SmothieDelight_RecruitmentAssignment.MvcApplication" Language="C#" %>
---
+++ SmothieDelight_RecruitmentAssignment/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SmothieDelight_RecruitmentAssignment.Startup))]
+namespace SmothieDelight_RecruitmentAssignment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
