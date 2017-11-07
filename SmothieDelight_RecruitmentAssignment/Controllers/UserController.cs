00be4736359698fca9234f5a61def2595889d438
AleksTor2208 <aleksander.toropowski@gmail.com>
2017-11-07 05:19:52 +0300

    Add UserController class implementing New method to add new user

---
+++ SmothieDelight_RecruitmentAssignment/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using SmothieDelight_RecruitmentAssignment.Models;
+using System.Web.Mvc;
+using System;
+
+namespace SmothieDelight_RecruitmentAssignment.Controllers
+{
+    public class UserController : Controller
+    {
+        ApplicationDbContext _context;
+
+        public UserController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult New(UserInfo user)
+        {
+            if (UserValidation(user))
+            { 
+                var newUser = new UserInfo
+                {
+                    FirstName = user.FirstName,
+                    LastName = user.LastName ?? null,
+                    Email = user.Email
+                };
+                _context.UsersInfo.Add(newUser);
+                _context.SaveChanges();
+                return Redirect("/Home/Index");
+            }
+            return Redirect("New");
+        }
+
+        private bool UserValidation(UserInfo user)
+        {
+            return user.FirstName != null
+                && user.Email != null;
+        }
+    }
+}
