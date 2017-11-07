39fa07367c09d6d8781815b02150f3decafba41c
AleksTor2208 <aleksander.toropowski@gmail.com>
2017-11-07 05:25:00 +0300

    Add SmoothieInfo in HomeController, modify ViewBag messages

--- SmothieDelight_RecruitmentAssignment/Controllers/HomeController.cs
+++ SmothieDelight_RecruitmentAssignment/Controllers/HomeController.cs
@@ -1,28 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using SmothieDelight_RecruitmentAssignment.Models;
 using System.Web.Mvc;
 
 namespace SmothieDelight_RecruitmentAssignment.Controllers
 {
     public class HomeController : Controller
-    {
+    {            
         public ActionResult Index()
         {
             return View();
         }
 
+        public ActionResult SmoothieInfo()
+        {
+            return View();
+        }       
+
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Smoothie Delight is a branch new product line including healthy and usefull supplements.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Feel free to contact us. Subscribe, so we could send you fresh news about our product!";
 
             return View();
         }
