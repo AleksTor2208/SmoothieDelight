6e767988a01cf599024376e9750fac92b7ff452e
AleksTor2208 <aleksander.toropowski@gmail.com>
2017-11-07 05:20:58 +0300

    Add UserInfo class

---
+++ SmothieDelight_RecruitmentAssignment/Models/UserInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SmothieDelight_RecruitmentAssignment.Models
+{
+    public class UserInfo
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Email { get; set; }
+    }
+}
